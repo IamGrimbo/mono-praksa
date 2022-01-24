4b552ad5e9d2f6ac63f1592bcf6bf24945214605
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-01-24 16:27:51 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static double AverageSubjectMark(ISchoolSubject Subject)
+        {
+            int sum = 0;
+            List<Student> students = new List<Student>();
+
+            Random random = new Random();
+
+            for (int i = Subject.amountOfStudentsTakingTheSubject; i > 0; i--)
+            {
+                students.Add(new Student() { mark = random.Next(1, 6) });
+            }
+
+            foreach (Student student in students)
+            {
+                sum += student.mark;
+            }
+
+            return (double)sum / (double)Subject.amountOfStudentsTakingTheSubject;
+        }
+        static void PrintSubjectAverageGrade(ISchoolSubject Subject)
+        {
+            Console.WriteLine("Total amount ff students taking {0} is {1}", Subject.subjectName, Subject.amountOfStudentsTakingTheSubject);
+        }
+        static void Main(string[] args)
+        {
+            ISchoolSubject Math = new SchoolSubject("Math", 27, "Ana");
+            double averageSubjectMark = AverageSubjectMark(Math);
+            
+            PrintSubjectAverageGrade(Math);
+            Console.WriteLine("Average Grade for {0} is {1}", Math.subjectName, averageSubjectMark);
+        }
+    }
+}
---
+++ SchoolSubject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class SchoolSubject : BestStudentAtSchoolSubject, ISchoolSubject
+    {
+		public SchoolSubject(string MsubjectName, int MamountOfStudentsTakingTheSubject, string MbestStudentAtThisSubject)
+		{
+			subjectName = MsubjectName;
+			amountOfStudentsTakingTheSubject = MamountOfStudentsTakingTheSubject;
+            bestStudentAtThisSubject = MbestStudentAtThisSubject;
+		}
+
+		public string subjectName { get; set; }
+
+		public int amountOfStudentsTakingTheSubject { get; set; }
+
+		public string bestStudentAtThisSubject { get; set; }
+
+		public override void BestStudentAtThisSubject()
+		{
+			Console.WriteLine("Best Student at {0} is {1}", subjectName, bestStudentAtThisSubject);
+		}
+    }
+}
