957a52c9f8c5d14305ae076763e17c3049597c7f
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-01-31 13:17:28 +0100

    Update Student.cs

--- Day_Three_To_Five/University.Models/Student.cs
+++ Day_Three_To_Five/University.Models/Student.cs
@@ -10,6 +10,9 @@ namespace University.Model
     public class Student : IStudent
     {
         public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
         public string IndexNumber { get; set; }
         public string Course { get; set; }
         public Person person { get; set; }
