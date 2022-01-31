58286db82250ac0087b7ed644efa19444f9b8d75
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-01-31 07:20:45 +0100

    Add files via upload

---
+++ Day_Three_To_Five/PersonRepository.cs
@@ -0,0 +1,146 @@
+﻿using University.Model;
+using University.Model.Common;
+using University.Repository.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Data.SqlClient;
+using System.Data;
+using System.Net;
+
+namespace University.Repository
+{
+    public class PersonRepository : IPersonRepository
+    {
+        public PersonRepository() { }
+
+        protected Person person = new Person();
+
+        static string connecitonString = "Server = localhost; Database = master; Trusted_Connection = True;";
+
+        public async Task<List<Person>> GetAllAsync()
+        {
+            List<Person> listOfPeople = new List<Person>();
+
+            string queryString = "SELECT * FROM person";
+
+            using (SqlConnection Connection = new SqlConnection(connecitonString))
+            {
+                Connection.Open();
+                SqlCommand Command = new SqlCommand(queryString, Connection);
+                SqlDataReader Reader = Command.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    Person person = new Person();
+                    person.Id = int.Parse(Reader["id"].ToString());
+                    person.FirstName = Reader["firstName"].ToString();
+                    person.LastName = Reader["lastName"].ToString();
+                    person.OIB = Reader["oib"].ToString();
+                    person.PlaceOfResidence = Reader["placeOfResidence"].ToString();
+                    person.StudentId = int.Parse(Reader["studentId"].ToString());
+                    listOfPeople.Add(person);
+                }
+                Reader.Close();
+                Connection.Close();
+                return listOfPeople;
+            }
+        }
+
+        public async Task<Person> GetByIdAsync(int id)
+        {
+
+            Person person = new Person();
+            string queryString = "SELECT * FROM person;";
+
+            using (SqlConnection Connection = new SqlConnection(connecitonString))
+            {
+
+                Connection.Open();
+                SqlCommand Command = new SqlCommand(queryString, Connection);
+                SqlDataReader Reader = Command.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    person.Id = int.Parse(Reader["id"].ToString());
+                    person.FirstName = Reader["firstName"].ToString();
+                    person.LastName = Reader["lastName"].ToString();
+                    person.OIB = Reader["oib"].ToString();
+                    person.PlaceOfResidence = Reader["placeOfResidence"].ToString();
+                    person.StudentId = int.Parse(Reader["studentId"].ToString());
+                }
+                Reader.Close();
+                Connection.Close();
+                return person;
+            }
+        }
+
+        public async Task<bool> PostAsync(Person person)
+        {
+            string queryString = "INSERT INTO person VALUES('" + person.FirstName + "','" + person.LastName + "','" + person.OIB + "','" + person.PlaceOfResidence + "','" + person.StudentId + "'); ";
+
+            using (SqlConnection Connection = new SqlConnection(connecitonString))
+            {
+                try
+                {
+                    Connection.Open();
+                    SqlCommand Command = new SqlCommand(queryString, Connection);
+
+                    Command.ExecuteNonQuery();
+                    Connection.Close();
+                    return true;
+                }
+                catch (SqlException sqlException)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public async Task<bool> PutAsync(int id, Person person)
+        {
+
+            string queryString = "UPDATE person SET firstName='" + person.FirstName + "', lastName='" + person.LastName + "', oib='" + person.OIB + "', placeOfResidence='" + person.PlaceOfResidence + "', studentId='" + person.StudentId + "'); ";
+
+            using (SqlConnection Connection = new SqlConnection(connecitonString))
+            {
+                try
+                {
+                    Connection.Open();
+                    SqlCommand Command = new SqlCommand(queryString, Connection);
+
+                    Command.ExecuteNonQuery();
+                    Connection.Close();
+                    return true;
+                }
+                catch (SqlException sqlException)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public async Task<bool> DeleteByIdAsync(int id)
+        {
+            string queryString = "DELETE FROM person WHERE id=" + id + ";";
+            using (SqlConnection Connection = new SqlConnection(connecitonString))
+            {
+                try
+                {
+                    Connection.Open();
+                    SqlCommand Command = new SqlCommand(queryString, Connection);
+                    Command.ExecuteNonQuery();
+                    Connection.Close();
+                    return true;
+                }
+                catch (SqlException sqlException)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ Day_Three_To_Five/StudentRepository.cs
@@ -0,0 +1,141 @@
+﻿using University.Model;
+using University.Model.Common;
+using University.Repository.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Data.SqlClient;
+using System.Data;
+using System.Net;
+
+namespace University.Repository
+{
+    public class StudentRepository : IStudentRepository
+    {
+        public StudentRepository() { }
+
+        protected Student student = new Student();
+
+        static string connecitonString = "Server = localhost; Database = master; Trusted_Connection = True;";
+
+        public async Task<List<Student>> GetAllAsync()
+        {
+            List<Student> listOfStudents = new List<Student>();
+
+            string queryString = "SELECT * FROM student";
+
+            using (SqlConnection Connection = new SqlConnection(connecitonString))
+            {
+                Connection.Open();
+                SqlCommand Command = new SqlCommand(queryString, Connection);
+                SqlDataReader Reader = Command.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    Student student = new Student();
+                    student.Id = int.Parse(Reader["id"].ToString());
+                    student.IndexNumber = Reader["indexNumber"].ToString();
+                    student.Course = Reader["course"].ToString();
+                    listOfStudents.Add(student);
+                }
+                Reader.Close();
+                Connection.Close();
+                return listOfStudents;
+            }
+        }
+
+        public async Task<Student> GetByIdAsync(int id)
+        {
+
+            Student student = new Student();
+
+            string queryString = "SELECT * FROM student WHERE id=" + id + ";";
+
+            using (SqlConnection Connection = new SqlConnection(connecitonString))
+            {
+
+                Connection.Open();
+                SqlCommand Command = new SqlCommand(queryString, Connection);
+                SqlDataReader Reader = Command.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    student.Id = int.Parse(Reader["id"].ToString());
+                    student.IndexNumber = Reader["indexNumber"].ToString();
+                    student.Course = Reader["course"].ToString();
+                }
+                Reader.Close();
+                Connection.Close();
+                return student;
+            }
+        }
+
+        public async Task<bool> PostAsync(Student student)
+        {
+            string queryString = "INSERT INTO student VALUES('" + student.IndexNumber + "','" + student.Course + "');";
+
+            using (SqlConnection Connection = new SqlConnection(connecitonString))
+            {
+                try
+                {
+                    Connection.Open();
+                    SqlCommand Command = new SqlCommand(queryString, Connection);
+
+                    Command.ExecuteNonQuery();
+                    Connection.Close();
+                    return true;
+                }
+                catch (SqlException sqlException)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public async Task<bool> PutAsync(int id, Student student)
+        {
+
+            string queryString = "UPDATE student SET indexnumber='" + student.IndexNumber + "', indexnumber='" + student.Course + "'WHERE id='" + id + "');";
+
+            using (SqlConnection Connection = new SqlConnection(connecitonString))
+            {
+                try
+                {
+                    Connection.Open();
+                    SqlCommand Command = new SqlCommand(queryString, Connection);
+
+                    Command.ExecuteNonQuery();
+                    Connection.Close();
+                    return true;
+                }
+                catch (SqlException sqlException)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public async Task<bool> DeleteByIdAsync(int id)
+        {
+            string queryString = "DELETE FROM student WHERE id=" + id + ";";
+            using (SqlConnection Connection = new SqlConnection(connecitonString))
+            {
+                try
+                {
+                    Connection.Open();
+                    SqlCommand Command = new SqlCommand(queryString, Connection);
+                    Command.ExecuteNonQuery();
+                    Connection.Close();
+                    return true;
+                }
+                catch (SqlException sqlException)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
