df6e5fafbf4a436b1f14d4c5c96a91783fdf7823
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-01-31 07:27:24 +0100

    Add files via upload

---
+++ Day_Three_To_Five/University.WebApi/PersonController.cs
@@ -0,0 +1,78 @@
+﻿using University.Model.Common;
+using University.Service;
+using University.Service.Common;
+using System;
+using System.Collections.Generic;
+using University.Model;
+using University.Webapi.Model;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading.Tasks;
+
+namespace University.WebApi.Controllers
+{
+    public class PersonController : ApiController
+    {
+        public PersonController() { }
+        protected PersonService Service = new PersonService();
+
+        [HttpGet]
+        public async Task<List<PersonViewModel>> GetAllAsync()
+        {
+            List<PersonViewModel> personViewList = new List<PersonViewModel>();
+            List<Person> personList = new List<Person>();
+            personList = await Service.GetAllAsync();
+
+            foreach (var person in personList)
+            {
+                PersonViewModel personViewModel = new PersonViewModel();
+                personViewModel.FirstName = person.FirstName;
+                personViewModel.LastName = person.LastName;
+                personViewModel.OIB = person.OIB;
+                personViewModel.DateofBirth = person.DateofBirth;
+                personViewModel.StudentId = person.StudentId;
+                personViewList.Add(personViewModel);
+            }
+            return personViewList;
+        }
+
+        [HttpGet]
+        public async Task<PersonViewModel> GetByIdAsync(int id)
+        {
+            PersonViewModel personViewModel = new PersonViewModel();
+            Person person = new Person();
+
+            person = await Service.GetByIdAsync(id);
+            personViewModel.FirstName = person.FirstName;
+            personViewModel.LastName = person.LastName;
+            personViewModel.OIB = person.OIB;
+            personViewModel.DateofBirth = person.DateofBirth;
+            personViewModel.StudentId = person.StudentId;
+
+            return personViewModel;
+        }
+
+        [HttpPost]
+        public async Task<bool> PostAsync(Person person)
+        {
+            return await Service.PostAsync(person);
+        }
+
+        [HttpPut]
+        public async Task<bool> PutAsync(int id, Person person)
+        {
+            return await Service.PutAsync(id, person);
+        }
+
+        [HttpDelete]
+        public async Task<bool> DeleteByIdAsync(int id)
+        {
+            return await Service.DeleteByIdAsync(id);
+        }
+
+    }
+}
---
+++ Day_Three_To_Five/University.WebApi/StudentController.cs
@@ -0,0 +1,72 @@
+﻿using University.Model;
+using University.Model.Common;
+using University.Service;
+using University.Service.Common;
+using System;
+using System.Collections.Generic;
+using University.Webapi.Model;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading.Tasks;
+
+namespace University.WebApi.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public StudentController() { }
+        protected IStudentService Service = new StudentService();
+
+        [HttpGet]
+        public async Task<List<StudentViewModel>> GetAllAsync()
+        {
+            List<StudentViewModel> studentViewList = new List<StudentViewModel>();
+            List<Student> studentList = new List<Student>();
+            studentList = await Service.GetAllAsync();
+
+            foreach (var student in studentList)
+            {
+                StudentViewModel studentViewModel = new StudentViewModel();
+                studentViewModel.IndexNumber = student.IndexNumber;
+                studentViewModel.Course = student.Course;
+                studentViewList.Add(studentViewModel);
+            }
+            return studentViewList;
+        }
+
+        [HttpGet]
+        public async Task<StudentViewModel> GetByIdAsync(int id)
+        {
+            Student student = new Student();
+            student = await Service.GetByIdAsync(id);
+
+            StudentViewModel studentViewModel = new StudentViewModel();
+            studentViewModel.IndexNumber = student.IndexNumber;
+            studentViewModel.Course = student.Course;
+            
+            return studentViewModel;
+        }
+
+        [HttpPost]
+        public async Task<bool> PostAsync(Student student)
+        {
+            return await Service.PostAsync(student);
+        }
+
+        [HttpPut]
+        public async Task<bool> PutAsync(int id, Student student)
+        {
+            return await Service.PutAsync(id, student);
+        }
+
+        [HttpDelete]
+        public async Task<bool> DeleteByIdAsync(int id)
+        {
+            return await Service.DeleteByIdAsync(id);
+        }
+
+    }
+}
