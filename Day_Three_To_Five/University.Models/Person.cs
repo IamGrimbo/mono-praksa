2dc9c05091235be468f3e7a641f1660c532c43f8
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-01-31 13:17:06 +0100

    Update Person.cs

--- Day_Three_To_Five/University.Models/Person.cs
+++ Day_Three_To_Five/University.Models/Person.cs
@@ -12,9 +12,9 @@ namespace University.Model
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Address { get; set; }
         public string OIB { get; set; }
         public string PlaceOfResidence { get; set; }
-        public string Address { get; set; }
         public DateTime DateofBirth { get; set; }
         public int StudentId { get; set; }
         public Student student { get; set; }
