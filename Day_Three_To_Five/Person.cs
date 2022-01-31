b3a226ed6256f57139ce4f4a87806e18d050906c
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-01-31 07:17:36 +0100

    Add files via upload

---
+++ Day_Three_To_Five/Person.cs
@@ -0,0 +1,20 @@
+﻿using University.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Model
+{
+    public class Person : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string OIB { get; set; }
+        public string PlaceOfResidence { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public int StudentId { get; set; }
+    }
+}
