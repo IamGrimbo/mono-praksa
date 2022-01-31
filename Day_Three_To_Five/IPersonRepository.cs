dc1987c47d27b185ffd5abfb1094a80126c4a4d0
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-01-31 07:21:37 +0100

    Add files via upload

---
+++ Day_Three_To_Five/IPersonRepository.cs
@@ -0,0 +1,19 @@
+﻿using University.Model;
+using University.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Repository.Common
+{
+    public interface IPersonRepository
+    {
+        Task<List<Person>> GetAllAsync();
+        Task<Person> GetByIdAsync(int id);
+        Task<bool> PostAsync(Person person);
+        Task<bool> PutAsync(int id, Person person);
+        Task<bool> DeleteByIdAsync(int id);
+    }
+}
---
+++ Day_Three_To_Five/IStudentRepository.cs
@@ -0,0 +1,19 @@
+﻿using University.Model;
+using University.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Repository.Common
+{
+    public interface IStudentRepository
+    {
+        Task<List<Student>> GetAllAsync();
+        Task<Student> GetByIdAsync(int id);
+        Task<bool> PostAsync(Student student);
+        Task<bool> PutAsync(int id, Student student);
+        Task<bool> DeleteByIdAsync(int id);
+    }
+}
