1046bdb602931c1ffe42ecd027dc3bfcc36fa988
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-01-31 13:16:18 +0100

    Update IStudent.cs

--- Day_Three_To_Five/University.Model.Common/IStudent.cs
+++ Day_Three_To_Five/University.Model.Common/IStudent.cs
@@ -9,6 +9,9 @@ namespace University.Model.Common
     public interface IStudent
     {
         int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Address { get; set; }
         string IndexNumber { get; set; }
         string Course { get; set; }
     }
