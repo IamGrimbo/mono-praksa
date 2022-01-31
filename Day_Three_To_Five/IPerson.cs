4b6420b3c3c4a7caf8211754897afeb1ed539f4c
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-01-31 07:19:26 +0100

    Add files via upload

---
+++ Day_Three_To_Five/IPerson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Model.Common
+{
+    public interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string OIB { get; set; }
+        string PlaceOfResidence { get; set; }
+        DateTime DateofBirth { get; set; }
+        int StudentId { get; set; }
+    }
+
+}
