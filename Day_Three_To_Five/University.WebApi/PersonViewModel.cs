230a401d42c0307c7f8651603a90d977322c8f94
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-01-31 07:28:27 +0100

    Add files via upload

---
+++ Day_Three_To_Five/University.WebApi/PersonViewModel.cs
@@ -0,0 +1,20 @@
+﻿using University.Model;
+using University.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Webapi.Model
+{
+    public class PersonViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string OIB { get; set; }
+        public string PlaceOfResidence { get; set; }
+        public DateTime DateofBirth { get; set; }
+        public int StudentId { get; set; }
+    }
+}
---
+++ Day_Three_To_Five/University.WebApi/StudentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using University.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Webapi.Model
+{
+    public class StudentViewModel
+    {
+        public string IndexNumber { get; set; }
+        public string Course { get; set; }
+    }
+}
