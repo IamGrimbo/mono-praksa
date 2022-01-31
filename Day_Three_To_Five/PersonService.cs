a3e8f6a1daa5af13169201f7065a98d951c3b922
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-01-31 07:22:59 +0100

    Add files via upload

---
+++ Day_Three_To_Five/PersonService.cs
@@ -0,0 +1,48 @@
+﻿using University.Model;
+using University.Model.Common;
+using University.Repository;
+using University.Repository.Common;
+using University.Service.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Service
+{
+    public class PersonService : IPersonService
+    {
+        public PersonService()
+        {
+        }
+
+        protected IPersonRepository Repository = new PersonRepository();
+
+        public async Task<List<Person>> GetAllAsync()
+        {
+            return await Repository.GetAllAsync();
+        }
+
+        public async Task<Person> GetByIdAsync(int id)
+        {
+            return await Repository.GetByIdAsync(id);
+        }
+
+        public async Task<bool> PostAsync(Person person)
+        {
+            return await Repository.PostAsync(person);
+        }
+
+        public async Task<bool> PutAsync(int id, Person person)
+        {
+            return await Repository.PutAsync(id, person);
+        }
+
+        public async Task<bool> DeleteByIdAsync(int id)
+        {
+            return await Repository.DeleteByIdAsync(id);
+        }
+
+    }
+}
---
+++ Day_Three_To_Five/StudentService.cs
@@ -0,0 +1,48 @@
+﻿using University.Model;
+using University.Model.Common;
+using University.Repository;
+using University.Repository.Common;
+using University.Service.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Service
+{
+    public class StudentService : IStudentService
+    {
+        public StudentService()
+        {
+        }
+
+        protected IStudentRepository Repository = new StudentRepository();
+
+        public async Task<List<Student>> GetAllAsync()
+        {
+            return await Repository.GetAllAsync();
+        }
+
+        public async Task<Student> GetByIdAsync(int id)
+        {
+            return await Repository.GetByIdAsync(id);
+        }
+
+        public async Task<bool> PostAsync(Student student)
+        {
+            return await Repository.PostAsync(student);
+        }
+
+        public async Task<bool> PutAsync(int id, Student student)
+        {
+            return await Repository.PutAsync(id, student);
+        }
+
+        public async Task<bool> DeleteByIdAsync(int id)
+        {
+            return await Repository.DeleteByIdAsync(id);
+        }
+
+    }
+}
