f17e2db909ba3ae6a12139d4078ab47d8ecfd306
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-02-02 06:02:22 +0100

    Add files via upload

---
+++ Page.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Common
+{
+    public class Page
+    {
+        public int ItemsPerPage { get; set; } = 20;
+        public int PageNumber { get; set; } = 1;
+        public string Paging()
+        {
+            if (PageNumber == 0 && ItemsPerPage == 0)
+                return String.Format("");
+
+            int Offset = (PageNumber - 1) * ItemsPerPage;
+
+            return String.Format(" OFFSET {0} ROWS FETCH NEXT {1} ROWS ONLY", Offset, ItemsPerPage);
+        }
+    }
+}
