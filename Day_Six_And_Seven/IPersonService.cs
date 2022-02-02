afcd32b33e1d4ab9dbf1e9e35d3e10fd354e5bc0
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-02-02 06:10:04 +0100

    Add files via upload

---
+++ Day_Six_And_Seven/IPersonService.cs
@@ -0,0 +1,19 @@
+﻿using University.Common;
+using University.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Service.Common
+{
+    public interface IPersonService
+    {
+        Task<List<IPerson>> GetAllAsync(PersonFilter filtering, PersonSort sorting, Page paging);
+        Task<IPerson> GetByIdAsync(int id);
+        Task PostAsync(IPerson person);
+        Task PutAsync(int id, IPerson person);
+        Task DeleteByIdAsync(int id);
+    }
+}
