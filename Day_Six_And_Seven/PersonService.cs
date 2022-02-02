02fe24e44d897c96a3512967610d931934eac38a
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-02-02 06:09:10 +0100

    Add files via upload

---
+++ Day_Six_And_Seven/PersonService.cs
@@ -0,0 +1,47 @@
+﻿using University.Common;
+using University.Model.Common;
+using University.Repository;
+using University.Repository.Common;
+using University.Service.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Service
+{
+    public class PersonService : IPersonService
+    {
+        public PersonService()
+        {
+        }
+
+        protected IPersonRepository Repository = new PersonRepository();
+
+        public async Task<List<IPerson>> GetAllAsync(PersonFilter filtering, PersonSort sorting, Page paging)
+        {
+            return await Repository.GetAllAsync(filtering, sorting, paging);
+        }
+
+        public async Task<IPerson> GetByIdAsync(int id)
+        {
+            return await Repository.GetByIdAsync(id);
+        }
+
+        public async Task PostAsync(IPerson person)
+        {
+            await Repository.PostAsync(person);
+        }
+
+        public async Task PutAsync(int id, IPerson person)
+        {
+            await Repository.PutAsync(id, person);
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            await Repository.DeleteByIdAsync(id);
+        }
+    }
+}
