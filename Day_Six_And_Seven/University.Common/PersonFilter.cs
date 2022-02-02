f3e93b527effe1b5936cb157a56c61950425829c
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-02-02 06:04:16 +0100

    Add files via upload

---
+++ Day_Six_And_Seven/University.Common/PersonFilter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Common
+{
+    public class PersonFilter
+    {
+        public string SearchBy { get; set; } = "";
+        public string Querry { get; set; } = "";
+        public string Filtering()
+        {
+            if ((SearchBy == null && SearchBy == "") || (Querry == null && Querry == ""))
+                return String.Format("");
+
+            return String.Format(" WHERE {0} LIKE '%{1}%'", SearchBy, Querry);
+        }
+    }
+}
