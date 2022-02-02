9c6aaecd29f244237c26e95b3c94ea94f773d7a7
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-02-02 06:08:10 +0100

    Add files via upload

---
+++ Day_Six_And_Seven/IStudentRepository.cs
@@ -0,0 +1,19 @@
+﻿using University.Common;
+using University.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Repository.Common
+{
+    public interface IStudentRepository
+    {
+        Task<List<IStudent>> GetAllAsync(StudentFilter filtering, StudentSort sorting, Page paging);
+        Task<IStudent> GetByIdAsync(int id);
+        Task PostAsync(IStudent student);
+        Task PutAsync(int id, IStudent student);
+        Task DeleteByIdAsync(int id);
+    }
+}
