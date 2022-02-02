cc2d8216df68b2da6573d11e8c1880c8acdedcc2
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-02-02 06:12:34 +0100

    Add files via upload

---
+++ Day_Six_And_Seven/University.WebApi/PersonViewModel.cs
@@ -0,0 +1,19 @@
+﻿using University.Model;
+using University.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Webapi.Model
+{
+    public class PersonViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PlaceOfResidence { get; set; }
+        public string Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
