290401e251014e4acbbc6d9b69b4cebacc291bad
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-02-03 06:13:35 +0100

    Add files via upload

---
+++ Day_Eight/University.WebApi/App_start/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace University.WebApi.App_start
+{
+    public class Bootstrapper
+    {
+
+        public static void Run()
+        {
+
+            AutofacWebapiConfig.Initialize(GlobalConfiguration.Configuration);
+        }
+
+    }
+}
