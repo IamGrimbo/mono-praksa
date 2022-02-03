2e1929da6386404e893ac5341538893a12ae2b77
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-02-03 06:13:05 +0100

    Add files via upload

---
+++ Day_Eight/AutofacWebapiConfig.cs
@@ -0,0 +1,43 @@
+﻿using University.Model;
+using University.Model.Common;
+using University.Repository;
+using University.Repository.Common;
+using University.Service;
+using University.Service.Common;
+using Autofac;
+using Autofac.Integration.WebApi;
+using System.Reflection;
+using System.Web.Http;
+
+namespace University.WebApi.App_start
+{
+    public class AutofacWebapiConfig
+    {
+        public static IContainer Container;
+
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterServices(new ContainerBuilder()));
+        }
+
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new ServiceDIModule());
+            builder.RegisterModule(new RepositoryDIModule());
+            builder.RegisterModule(new ModelDIModule());
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            Container = builder.Build();
+
+            return Container;
+
+            return Container;
+        }
+    }
+}
