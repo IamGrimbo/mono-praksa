96bc12a222b10b092a9d736b5f39a30e67676dea
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-02-03 06:09:43 +0100

    Add files via upload

---
+++ Day_Eight/IPersonRepository.cs
@@ -0,0 +1,19 @@
+﻿using University.Common;
+using University.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Repository.Common
+{
+    public interface IPersonRepository
+    {
+        Task<List<IPerson>> GetAllAsync(PersonFilter filtering, PersonSort sorting, Page paging);
+        Task<IPerson> GetByIdAsync(int id);
+        Task PostAsync(IPerson person);
+        Task PutAsync(int id, IPerson person);
+        Task DeleteByIdAsync(int id);
+    }
+}
