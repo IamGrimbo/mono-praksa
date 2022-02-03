d396957aedbfc1dedbce9084e5a7b6aa0330d748
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-02-03 06:05:17 +0100

    Add files via upload

---
+++ Day_Eight/University.Repository/RepositoryDIModule.cs
@@ -0,0 +1,25 @@
+﻿using University.Repository.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+
+namespace University.Repository
+{
+    public class RepositoryDIModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<StudentRepository>()
+                       .As<IStudentRepository>()
+                       .InstancePerRequest();
+
+            builder.RegisterType<PersonRepository>()
+                       .As<IPersonRepository>()
+                       .InstancePerRequest();
+        }
+    }
+}
---
+++ Day_Eight/University.Repository/StudentRepository.cs
@@ -0,0 +1,123 @@
+﻿using University.Common;
+using University.Model;
+using University.Model.Common;
+using University.Repository.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Data.SqlClient;
+using System.Data;
+using System.Net;
+
+namespace University.Repository
+{
+    public class StudentRepository : IStudentRepository
+    {
+        public StudentRepository() { }
+
+        protected IStudent student = new Student();
+
+        static string connectionString = "Server = localhost; Database = master; Trusted_Connection = True;";
+
+        public async Task<List<IStudent>> GetAllAsync(StudentFilter filtering, StudentSort sorting, Page paging)
+        {
+            List<IStudent> listOfStudents = new List<IStudent>();
+
+            string queryString = "SELECT * FROM student;" + filtering.Filtering() + sorting.Sorting() + paging.Paging();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(queryString, connection);
+                SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                while (reader.Read())
+                {
+                    Student student = new Student();
+                    student.Id = int.Parse(reader["id"].ToString());
+                    student.IndexNumber = reader["indexNumber"].ToString();
+                    student.Course = reader["course"].ToString();
+                    listOfStudents.Add(student);
+                }
+
+                reader.Close();
+                connection.Close();
+            }
+
+            return listOfStudents;
+        }
+
+        public async Task<IStudent> GetByIdAsync(int id)
+        {
+
+            IStudent student = new Student();
+
+            string queryString = "SELECT * FROM student WHERE id=" + id + ";";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                connection.Open();
+                SqlCommand command = new SqlCommand(queryString, connection);
+                SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                while (reader.Read())
+                {
+                    student.Id = int.Parse(reader["id"].ToString());
+                    student.IndexNumber = reader["indexNumber"].ToString();
+                    student.Course = reader["course"].ToString();
+                }
+
+                reader.Close();
+                connection.Close();
+            }
+
+            return student;
+        }
+
+        public async Task PostAsync(IStudent student)
+        {
+            string queryString = "INSERT INTO student VALUES('" + student.IndexNumber + "','" + student.Course + "');";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                await command.ExecuteNonQueryAsync();
+                connection.Close();
+            }
+        }
+
+        public async Task PutAsync(int id, IStudent student)
+        {
+
+            string queryString = "UPDATE student SET indexnumber='" + student.IndexNumber + "', course='" + student.Course + "'WHERE id='" + id + "');";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                await command.ExecuteNonQueryAsync();
+                connection.Close();
+            }
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            string queryString = "DELETE FROM student WHERE id=" + id + ";";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                await command.ExecuteNonQueryAsync();
+                connection.Close();
+            }
+        }
+    }
+}
