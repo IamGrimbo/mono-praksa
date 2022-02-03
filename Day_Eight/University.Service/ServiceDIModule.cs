6887fa5465a0c657e65163abb18f35e1cb7b0983
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-02-03 06:11:17 +0100

    Add files via upload

---
+++ Day_Eight/University.Service/ServiceDIModule.cs
@@ -0,0 +1,25 @@
+﻿using University.Service.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+
+namespace University.Service
+{
+    public class ServiceDIModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<StudentService>()
+                       .As<IStudentService>()
+                       .InstancePerRequest();
+
+            builder.RegisterType<PersonService>()
+                       .As<IPersonService>()
+                       .InstancePerRequest();
+        }
+    }
+}
---
+++ Day_Eight/University.Service/StudentService.cs
@@ -0,0 +1,47 @@
+﻿using University.Common;
+using University.Model.Common;
+using University.Repository;
+using University.Repository.Common;
+using University.Service.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Service
+{
+    public class StudentService : IStudentService
+    {
+        public StudentService()
+        {
+        }
+
+        protected IStudentRepository Repository = new StudentRepository();
+
+        public async Task<List<IStudent>> GetAllAsync(StudentFilter filtering, StudentSort sorting, Page paging)
+        {
+            return await Repository.GetAllAsync(filtering, sorting, paging);
+        }
+
+        public async Task<IStudent> GetByIdAsync(int id)
+        {
+            return await Repository.GetByIdAsync(id);
+        }
+
+        public async Task PostAsync(IStudent student)
+        {
+            await Repository.PostAsync(student);
+        }
+
+        public async Task PutAsync(int id, IStudent student)
+        {
+            await Repository.PutAsync(id, student);
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            await Repository.DeleteByIdAsync(id);
+        }
+    }
+}
