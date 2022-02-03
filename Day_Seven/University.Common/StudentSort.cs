19bfaac054f3f8f073a8151a65606fbfef5150ab
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-02-03 05:52:31 +0100

    Add files via upload

---
+++ Day_Seven/University.Common/StudentSort.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Common
+{
+    public class StudentSort
+    {
+        public string SortBy { get; set; } = "course";
+        public string SortOrder { get; set; } = "ASC";
+        public string Sorting()
+        {
+            if ((SortBy == null && SortBy == "") || (SortOrder == null && SortOrder == ""))
+                return String.Format("");
+
+            return String.Format(" ORDER BY {0} {1}", SortBy, SortOrder);
+        }
+    }
+}
