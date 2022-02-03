9ab2ec3932855c785027876ec2a0092cb2ceac3d
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-02-03 05:53:52 +0100

    Add files via upload

---
+++ Day_Seven/Student.cs
@@ -0,0 +1,16 @@
+﻿using University.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Model
+{
+    public class Student : IStudent
+    {
+        public int Id { get; set; }
+        public string IndexNumber { get; set; }
+        public string Course { get; set; }
+    }
+}
