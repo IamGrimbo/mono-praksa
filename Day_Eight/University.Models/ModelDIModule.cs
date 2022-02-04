eed75169e9f374ec9de145bbf713d5fc56560046
Kristian Rücker <98309427+IamGrimbo@users.noreply.github.com>
2022-02-04 09:51:30 +0100

    Add files via upload

---
+++ Day_Eight/University.Models/ModelDIModule.cs
@@ -0,0 +1,25 @@
+﻿using University.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+
+namespace University.Model
+{
+    public class ModelDIModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Student>()
+                       .As<IStudent>()
+                       .InstancePerRequest();
+
+            builder.RegisterType<Person>()
+                       .As<IPerson>()
+                       .InstancePerRequest();
+        }
+    }
+}
